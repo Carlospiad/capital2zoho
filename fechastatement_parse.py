891e94d384d6dc9c3142190e729482cf6e1a7a45
Carlos Piad <carlospiad@gmail.com>
2018-07-20 16:30:17 -0500

    working commit

--- fechastatement_parse.py
+++ fechastatement_parse.py
@@ -3,50 +3,69 @@ import csv
 from datetime import datetime
 
 
-book = xlrd.open_workbook("data/data.xls")
+book = xlrd.open_workbook("data/data.xlsx")
 sh = book.sheet_by_index(0)
 
 empty_cell= False
-with xlrd.open_workbook("data/data.xls") as wb:
+
+with xlrd.open_workbook("data/data.xlsx") as wb:
     with open('data/output.csv', 'w+') as csvout:
         cs= wb.sheet_by_index(0)
         num_cols= cs.ncols
         num_rows= cs.nrows
         writer = csv.writer(csvout, lineterminator='\n')
-        writer.writerow(['Date', 'Payee','Conepto','Referencia', 'Outflow', 'Inflow']) # write new header
-        for row_index in range(12, num_rows):
-            # set count empty cells
-            count_empty = 0
-            print('Row: {}'.format(row_index))
-            for col_index in range(0,num_cols):
-                # get cell value
-                cell_val= cs.cell(row_index, col_index).value
-                # check if cell is empty
-                if cell_val== '':
-                    # set empty cell is True
-                    empty_cell = True
-                    # increment counter
-                    count_empty+= 1
-                else:
-                    # set empty cell is false
-                    empty_cell= False
-
-                # check if cell is not empty
-                if not empty_cell:
-                    # print value of cell
-                    print('Col #: {} | Value: {}'.format(col_index, cell_val))
-
-
-            # check the counter if is = num_cols means the whole row is empty
-            if count_empty == num_cols:
-                print ('Row is empty')
-                # stop looping to next rows
-                break
+        writer.writerow(['Date', 'Payee','Concepto','Referencia', 'Outflow', 'Inflow']) # write new header
+        for row_index in range(1, num_rows):
             writer.writerow((
-                sh.cell_value(row_index, colx=1), #fecha
-                sh.cell_value(row_index, colx=6),
-                sh.cell_value(row_index, colx=11),
-                sh.cell_value(row_index, colx=16),
-                sh.cell_value(row_index, colx=18),
-                sh.cell_value(row_index, colx=23)
+                sh.cell_value(row_index, colx=0), #fecha
+                '',
+                sh.cell_value(row_index, colx=3),
+                sh.cell_value(row_index, colx=1),
+                sh.cell_value(row_index, colx=7),
+                sh.cell_value(row_index, colx=8)
                 ))
+
+#
+# with xlrd.open_workbook("data/data.xlsx") as wb:
+#     with open('data/output.csv', 'w+') as csvout:
+#         cs= wb.sheet_by_index(0)
+#         num_cols= cs.ncols
+#         num_rows= cs.nrows
+#         writer = csv.writer(csvout, lineterminator='\n')
+#         writer.writerow(['Date', 'Payee','Conepto','Referencia', 'Outflow', 'Inflow']) # write new header
+#         for row_index in range(12, num_rows):
+#             # set count empty cells
+#             count_empty = 0
+#             print('Row: {}'.format(row_index))
+#             for col_index in range(0,num_cols):
+#                 # get cell value
+#                 cell_val= cs.cell(row_index, col_index).value
+#                 # check if cell is empty
+#                 if cell_val== '':
+#                     # set empty cell is True
+#                     empty_cell = True
+#                     # increment counter
+#                     count_empty+= 1
+#                 else:
+#                     # set empty cell is false
+#                     empty_cell= False
+#
+#                 # check if cell is not empty
+#                 if not empty_cell:
+#                     # print value of cell
+#                     print('Col #: {} | Value: {}'.format(col_index, cell_val))
+#
+#
+#             # check the counter if is = num_cols means the whole row is empty
+#             if count_empty == num_cols:
+#                 print ('Row is empty')
+#                 # stop looping to next rows
+#                 break
+#             writer.writerow((
+#                 sh.cell_value(row_index, colx=1), #fecha
+#                 sh.cell_value(row_index, colx=6),
+#                 sh.cell_value(row_index, colx=11),
+#                 sh.cell_value(row_index, colx=16),
+#                 sh.cell_value(row_index, colx=18),
+#                 sh.cell_value(row_index, colx=23)
+#                 ))
