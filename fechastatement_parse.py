dd086c8fc3ad1a83bf80ea1ca7548064b5292efa
Carlos Piad <carlospiad@gmail.com>
2018-09-18 13:33:23 -0500

    remove "-" from output

--- fechastatement_parse.py
+++ fechastatement_parse.py
@@ -16,11 +16,31 @@ with xlrd.open_workbook("data/data.xlsx") as wb:
         writer = csv.writer(csvout, lineterminator='\n')
         writer.writerow(['Date', 'Payee','Concepto','Referencia', 'Outflow', 'Inflow']) # write new header
         for row_index in range(1, num_rows):
+
             writer.writerow((
                 sh.cell_value(row_index, colx=0), #fecha
                 '',
                 sh.cell_value(row_index, colx=3),
                 sh.cell_value(row_index, colx=1),
-                sh.cell_value(row_index, colx=7),
-                sh.cell_value(row_index, colx=8)
+                "" if sh.cell_value(row_index, colx=7)=='-' else sh.cell_value(row_index, colx=7),
+                "" if sh.cell_value(row_index, colx=8)=='-' else sh.cell_value(row_index, colx=8)
                 ))
+
+
+# with xlrd.open_workbook("data/data.xlsx") as wb:
+#     with open('data/output.csv', 'w+') as csvout:
+#         cs= wb.sheet_by_index(0)
+#         num_cols= cs.ncols
+#         num_rows= cs.nrows
+#         writer = csv.writer(csvout, lineterminator='\n')
+#         writer.writerow(['Date', 'Payee','Concepto','Referencia', 'Outflow', 'Inflow']) # write new header
+#         for row_index in range(1, num_rows):
+#
+#             writer.writerow((
+#                 sh.cell_value(row_index, colx=0), #fecha
+#                 '',
+#                 sh.cell_value(row_index, colx=3),
+#                 sh.cell_value(row_index, colx=1),
+#                 if sh.cell_value(row_index, colx=7)='-',
+#                 sh.cell_value(row_index, colx=8)
+#                 ))
